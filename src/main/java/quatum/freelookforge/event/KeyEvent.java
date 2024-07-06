a62d518aeda92d6ded0509642d56643396c9b498
Quantum-mutnauQ <141065355+Quantum-mutnauQ@users.noreply.github.com>
2024-07-06 11:07:03 +0200

    Upside down fix
    · fixed when upside down is enabled an you are syncing your head with your camera, than your head in mincraft is upside down too.

---
+++ src/main/java/quatum/freelookforge/event/KeyEvent.java
@@ -0,0 +1,52 @@
+package quatum.freelookforge.event;
+
+import net.minecraft.client.CameraType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.util.Mth;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import quatum.freelookforge.CameraLook;
+import quatum.freelookforge.FreeLookForge;
+import quatum.freelookforge.KeyBinding.FreeLookKey;
+
+public class KeyEvent {
+    @Mod.EventBusSubscriber(modid = FreeLookForge.MODID,value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvent{
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event){
+            event.register(FreeLookKey.FREELOOK_KAY);
+            event.register(FreeLookKey.PERSPECTIVESWAP_KAY);
+        }
+    }
+    @Mod.EventBusSubscriber(modid = FreeLookForge.MODID,value = Dist.CLIENT)
+    public static class ClientForgeEvents{
+        @SubscribeEvent
+        public static void onInput(InputEvent event) {
+            CameraLook cameraLook = CameraLook.instance;
+            cameraLook.setSwapActive(FreeLookKey.PERSPECTIVESWAP_KAY.isDown());
+            cameraLook.setLookActive(FreeLookKey.FREELOOK_KAY.isDown());
+            cameraLook.setSyncAttack(Minecraft.getInstance().options.keyAttack.isDown());
+            cameraLook.setSyncPickItem(Minecraft.getInstance().options.keyPickItem.isDown());
+            cameraLook.setSyncUse(Minecraft.getInstance().options.keyUse.isDown());
+            CameraType type =Minecraft.getInstance().options.getCameraType();
+
+            cameraLook.SwapPerspective();
+                if(cameraLook.shouldSync(type)&&cameraLook.isCamaraMode(type)){
+                    LocalPlayer LP = Minecraft.getInstance().player;
+                    if(Minecraft.getInstance().options.getCameraType() == CameraType.THIRD_PERSON_FRONT){
+                        LP.setXRot(cameraLook.convertToAngle(cameraLook.getXRot())*-1);
+                        LP.setYRot(cameraLook.getYRot()-180);
+                    }else {
+                        LP.setXRot(cameraLook.convertToAngle(cameraLook.getXRot()));
+                        LP.setYRot(cameraLook.getYRot());
+                    }
+            }
+                cameraLook.SwapPerspectiveBack();
+                cameraLook.SwitchPerspectiveBack();
+        }
+    }
+}
