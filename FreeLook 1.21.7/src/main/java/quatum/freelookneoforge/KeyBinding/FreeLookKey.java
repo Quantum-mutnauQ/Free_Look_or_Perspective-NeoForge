3e6c0a503e8ec2ebd8852ee49aaef03f2fb4536b
Quantum-mutnauQ <unconfigured@null.spigotmc.org>
2025-07-01 16:23:34 +0200

    added support for 1.21.7

---
+++ FreeLook 1.21.7/src/main/java/quatum/freelookneoforge/KeyBinding/FreeLookKey.java
@@ -0,0 +1,21 @@
+package quatum.freelookneoforge.KeyBinding;import net.neoforged.fml.common.Mod;
+
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+
+import net.neoforged.neoforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+import java.awt.*;
+
+public class FreeLookKey {
+    public static final String KEY_CATEGORY_FREELOOK = "key.category.freelook";
+    public static final String KEY_FREE_LOOK = "key.freelookneoforge.freelook";
+    public static final String KEY_PERSPECTIVE_SWAP = "key.freelookneoforge.perspectiveswap";
+
+
+    public static final KeyMapping FREELOOK_KAY = new KeyMapping(KEY_FREE_LOOK, KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_F6,KEY_CATEGORY_FREELOOK);
+    public static final KeyMapping PERSPECTIVESWAP_KAY = new KeyMapping(KEY_PERSPECTIVE_SWAP, KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_F7,KEY_CATEGORY_FREELOOK);
+
+}
